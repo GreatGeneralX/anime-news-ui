601a9ecb2ba8a969099ae28861271345860b1e79
GreatGeneralX <hqrut0tsu4@gmail.com>
2025-04-06 23:51:01 +0900

    💅 CSS再適用＆美しさを取り戻す

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -32,7 +32,8 @@ export default function Layout() {
       <Sidebar isOpen={sidebarOpen} />
   
       {/* メイン */}
-      <main className="ml-0 sm:ml-56 h-full overflow-y-auto px-6 py-6 w-full max-w-none">
+      <main className="h-full overflow-y-auto px-6 py-6 w-full max-w-none">
+
 
         <Outlet />
       </main>
