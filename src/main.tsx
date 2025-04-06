0c98d7071e33a180876f398cfc7f74a2b22b24cb
GreatGeneralX <hqrut0tsu4@gmail.com>
2025-04-06 18:46:47 +0900

    💫 修正：tsx拡張子のimport削除

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 // src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx' // ← ここがちゃんと `App.tsx` 指してる？
+import App from './App'; 
+// ← ここがちゃんと `App.tsx` 指してる？
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
