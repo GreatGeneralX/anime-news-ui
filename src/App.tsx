2c635c6608c6becbecbe36456e2c11e8674ece1d
GreatGeneralX <hqrut0tsu4@gmail.com>
2025-04-06 21:59:28 +0900

    💅 CSS再適用＆美しさを取り戻す

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import FavoritesPage from './pages/FavoritesPage';
 import Layout from './components/Layout';
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="favorites" element={<FavoritesPage />} />
         </Route>
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
