80ef085df9fd6ba9573742718804945471510b90
GreatGeneralX <hqrut0tsu4@gmail.com>
2025-04-06 21:21:18 +0900

    💅 CSS再適用＆美しさを取り戻す

---
+++ src/pages/FavoritesPage.tsx
@@ -0,0 +1,9 @@
+export default function FavoritesPage() {
+    return (
+      <div className="text-center py-20">
+        <h1 className="text-3xl font-bold">お気に入りページ</h1>
+        <p className="text-gray-600 mt-4">ここにお気に入りの記事だけを表示する予定だよ♪</p>
+      </div>
+    );
+  }
+  
