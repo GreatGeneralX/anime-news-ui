39b4d48ce34675c144d9d66a57917461f8583a71
GreatGeneralX <hqrut0tsu4@gmail.com>
2025-04-06 18:51:54 +0900

    💅 CSS再適用＆美しさを取り戻す

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'; 
+import './index.css';
 // ← ここがちゃんと `App.tsx` 指してる？
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
