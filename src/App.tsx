07fe811165ea946b10afa7345375b27c4f2c5f5d
GreatGeneralX <hqrut0tsu4@gmail.com>
2025-04-07 13:36:30 +0900

    💅 CSS再適用＆美しさを取り戻す

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import {
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import FavoritesPage from './pages/FavoritesPage';
-import AccountOverlay from './pages/AccountOverlay';
 import Layout from './components/Layout';
 
 export default function App() {
@@ -20,25 +19,19 @@ export default function App() {
 
 function MainRoutes() {
   const location = useLocation();
-  const state = location.state as { backgroundLocation?: Location };
+
+  const showAccountOverlay = location.pathname === '/account';
 
   return (
-    <>
-      {/* 通常レイアウト */}
-      <Routes location={state?.backgroundLocation || location}>
-        <Route path="/" element={<Layout />}>  
-          <Route index element={<HomePage />} />
-          <Route path="favorites" element={<FavoritesPage />} />
-          <Route path="shop" element={<div className="mt-16">ショップページだよ🛍️</div>} />
-        </Route>
-      </Routes>
-
-      {/* アカウントはオーバーレイ表示のみ追加で */}
-      {state?.backgroundLocation && (
-        <Routes>
-          <Route path="/account" element={<AccountOverlay />} />
-        </Routes>
-      )}
-    </>
+    <Routes location={location}>
+      <Route
+        path="/"
+        element={<Layout showAccountOverlay={showAccountOverlay} />}
+      >
+        <Route index element={<HomePage />} />
+        <Route path="favorites" element={<FavoritesPage />} />
+        <Route path="shop" element={<div className="mt-16">ショップページだよ🛍️</div>} />
+      </Route>
+    </Routes>
   );
 }
