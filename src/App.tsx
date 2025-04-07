a38f537e4e90b5185230a083d4a8e49d46a4f91d
GreatGeneralX <hqrut0tsu4@gmail.com>
2025-04-07 17:01:30 +0900

    💅 CSS再適用＆美しさを取り戻す

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,31 @@ import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import FavoritesPage from './pages/FavoritesPage';
+import AccountOverlay from './pages/AccountOverlay';
 
-function MainRoutes() {
+function App() {
   const location = useLocation();
   const state = location.state as { backgroundLocation?: Location };
 
-  const isAccount = location.pathname === '/account';
+  const isOverlay = location.pathname === "/account";
 
   return (
     <>
       <Routes location={state?.backgroundLocation || location}>
-        <Route path="/" element={<Layout showAccountOverlay={isAccount} />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="favorites" element={<FavoritesPage />} />
-          <Route path="shop" element={<div className="mt-16">ショップページだよ🛒</div>} />
+          <Route path="shop" element={<div className="mt-16">ショップページだよ🛍️</div>} />
         </Route>
       </Routes>
+
+      {(state?.backgroundLocation || isOverlay) && (
+        <Routes>
+          <Route path="/account" element={<AccountOverlay />} />
+        </Routes>
+      )}
     </>
   );
 }
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <MainRoutes />
-    </BrowserRouter>
-  );
-}
+export default App;
