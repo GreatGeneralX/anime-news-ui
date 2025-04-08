e7e7e8d882f5007adaa96620e8cd5618420fd50a
GreatGeneralX <hqrut0tsu4@gmail.com>
2025-04-08 13:59:58 +0900

    🐛 fix: Router context error

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-// src/main.tsx
+// main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'; 
-import './index.css';
-// ← ここがちゃんと `App.tsx` 指してる？
+import App from './App'
+import './index.css'
+
+// Router を追加
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
-
-
