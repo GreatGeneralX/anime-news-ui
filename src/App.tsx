a26081446efafce781671cacc6bbd845732ed681
GreatGeneralX <hqrut0tsu4@gmail.com>
2025-04-08 20:41:26 +0900

    🐛 fix: Router context error

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,34 @@ import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import FavoritesPage from './pages/FavoritesPage';
-import AccountOverlay from './pages/AccountOverlay';
+// import AccountOverlay from './pages/AccountOverlay';
+import AccountPage from './pages/AccountPage'; // ← これを追加！
 
 export default function App() {
   const location = useLocation();
-  const state = location.state as { backgroundLocation?: Location };
-
-  const background = state?.backgroundLocation;
+  // const state = location.state as { backgroundLocation?: Location };
+  // const background = state?.backgroundLocation;
 
   return (
     <>
       {/* 通常ページ用のルーティング */}
-      <Routes location={background || location}>
-        <Route path="/" element={<Layout showAccountOverlay={!!background} />}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="favorites" element={<FavoritesPage />} />
           <Route path="shop" element={<div className="mt-16">ショップページだよ🛍️</div>} />
+          <Route path="account" element={<AccountPage />} /> {/* ← 通常表示に変更 */}
         </Route>
       </Routes>
 
-      {/* オーバーレイ（アカウント）だけ追加で表示 */}
+      {/* オーバーレイは一旦OFF */}
+      {/*
       {background && (
         <Routes>
           <Route path="/account" element={<AccountOverlay />} />
         </Routes>
       )}
+      */}
     </>
   );
 }
