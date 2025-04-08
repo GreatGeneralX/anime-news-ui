5db5beae3ac50799255f8b3cab618a8f03ff1762
GreatGeneralX <hqrut0tsu4@gmail.com>
2025-04-08 21:43:52 +0900

    🐛 fix: Router context error

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { AccountOverlayProvider } from './pages/AccountOverlayContext';
+// main.tsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+// Router を追加
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AccountOverlayProvider>
-        <App />
-      </AccountOverlayProvider>
+      <App />
     </BrowserRouter>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
